5a9a5f4e87826aee86d58ae4551aa34f9c8a6291
Baasit <95265491+abdulbaasitt@users.noreply.github.com>
2023-10-29 15:32:24 +0000

    First Commit

--- README.md
+++ README.md
@@ -41,3 +41,141 @@ Then, add *C:\vcpkg\installed\x64-windows\bin* and *C:\vcpkg\installed\x64-windo
 2. Make a build directory in the top level directory: `mkdir build && cd build`
 3. Compile: `cmake .. && make`
 4. Run it: `./2D_feature_tracking`.
+
+## Steps
+
+### MP.1 Data Buffer Optimization
+
+Construct a vector for dataBuffer objects with a size constraint (e.g., 2 elements), by adding new elements at one end while eliminating elements at the other end.
+
+
+### MP.2 Keypoint Detection
+
+Incorporate detectors such as HARRIS, FAST, BRISK, ORB, AKAZE, and SIFT, and ensure their selection via setting a string accordingly.
+
+
+### MP.3 Keypoint Removal
+
+Eliminate all keypoints outside a pre-defined rectangle, retaining only the keypoints within the rectangle for further processing.
+
+### MP.4 Keypoint Descriptors
+
+Employ descriptors such as BRIEF, ORB, FREAK, AKAZE and SIFT, and enable their selection by setting a string accordingly.
+
+
+### MP.5 Descriptor Matching
+
+Implement FLANN matching along with k-nearest neighbor selection. Both methods should be selectable through their respective strings in the main function.
+
+
+### MP.6 Descriptor Distance Ratio
+
+Utilize K-Nearest-Neighbor matching to execute the descriptor distance ratio test, which evaluates the ratio of the best versus second-best match to determine whether to retain a pair of associated keypoints.
+
+
+### MP.7  Performance Evaluation 1
+
+#### Keypoints Counting
+
+Count the number of keypoints on the preceding vehicle across all 10 images, utilizing the different detectors that have been implemented. The table below shows the average time and number of keypoints for different detectors.
+
+It's clear that **FAST** provides the best detection speed and fairly good accuracy.
+
+| Detector   | Average time (ms) | Average keypoints amount |
+| :--------- | ----------------- | ------------------------ |
+| Harris     | 15.5229           | 24.8                     |
+| SHI-TOMASI | 15.1395           | 78.2                     |
+| **FAST**   | **1.24024**       | **149.1**                |
+| BRISK      | **264.814**       | **276.2**                |
+| ORB        | 25.4274           | 116.1                    |
+| AKAZE      | 55.6996           | 167                      |
+| SIFT       | 63.2617           | 138.7                    |
+
+#### Neighborhood Distribution
+
+As depicted in the figure below, **Harris, Shi-Tomasi, and FAST** have relatively small neighborhood size and spatial distribution with no overlapping.
+
+On the contrary, **BRISK and ORB** exhibit a very large neighborhood size and compact distribution resembling clusters with many overlapping areas.
+
+Lastly, **AKAZE and SIFT** possess a medium neighborhood size and relatively uniform distribution with minimal overlapping.
+
+
+### MP.8 & MP.9 Performance Evaluation 2&3
+
+Calculate the number of matched keypoints across all 10 images using all possible combinations of detectors and descriptors. In the matching phase, the BF approach is utilized with the descriptor distance ratio set to 0.8.
+
+Record the time taken for keypoint detection and descriptor extraction. The results should be logged into a spreadsheet, and based on this data, the TOP3 detector/descriptor combinations are recommended for the specific goal of detecting keypoints on vehicles.
+
+#### NOTES
+
+- BINARY descriptors :BRISK, BRIEF, ORB, FREAK, and AKAZE ----NORM_L2
+- HOG descriptors : SIFT (and SURF and GLOH, all patented) ---- NORM_HAMMING
+- NORM_HAMMING2 should be used with ORB when WTA_K==3 or 4
+- Some combinations resulted in errors:
+  - KAZE/AKAZE descriptors will only work with KAZE/AKAZE detectors.
+  - SIFT detector and ORB descriptor do not work together
+
+- The matcher is "brute force (BF)" and selector is"k nearest neighbors (KNN)".
+- Only keypoints on the preceding vehicle in image are processed.
+
+
+
+The results of all possible combinations of detectors and descriptors are shown below, with the results being averaged processing time and number of keypoints for each step. Note, there are 10 detector results, 10 descriptor results, but only 9 matcher results, as keypoints matching requires 2 frame images simultaneously.
+
+| Descriptor extraction   | Average time |    (ms)     |         |         |         |
+| :---------------------- | :----------: | :---------: | :-----: | :-----: | :-----: |
+| **Detector/Descriptor** |    BRIEF     |     ORB     |  FREAK  |  AKAZE  |  SIFT   |
+| SHI-TOMASI              |   0.988691   |  0.970024   | 34.8317 |    X    | 11.7726 |
+| HARRIS                  |   0.52873    |   0.82866   | 35.7314 |    X    | 10.5432 |
+| FAST                    | **1.27674**  | **1.34534** | 36.0595 |    X    | 13.9776 |
+| BRISK                   | **0.993699** |   4.50749   | 33.6946 |    X    | 23.5365 |
+| ORB                     |   0.653052   |   4.97422   | 34.6063 |    X    | 28.4616 |
+| AKAZE                   |   0.742632   |   3.20843   | 37.2606 | 45.8597 | 16.7173 |
+| SIFT                    |   1.03263    |      X      | 38.4262 |    X    | 53.3684 |
+
+
+
+| Matched keypoints       |   Average   | number  |         |         |         |
+| ----------------------- | :---------: | :-----: | :-----: | :-----: | :-----: |
+| **Detector/Descriptor** |    BRIEF    |   ORB   |  FREAK  |  AKAZE  |  SIFT   |
+| SHI-TOMASI              |   60.7778   | 56.7778 | 40.5556 |    X    | 70.1111 |
+| HARRIS                  |   19.2222   |   18    |   16    |    X    | 18.1111 |
+| FAST                    | **122.111** | **119** | 97.5556 |    X    | 116.222 |
+| BRISK                   | **189.333** | 168.222 | 169.333 |    X    | 182.889 |
+| ORB                     |   60.5556   | 84.7778 | 46.6667 |    X    | 84.7778 |
+| AKAZE                   |   140.667   | 131.333 | 131.889 | 130.222 | 141.111 |
+| SIFT                    |   78.2222   |    X    | 66.1111 |    X    | 89.1111 |
+
+
+
+| Total time              | (ms)        |             |         |         |         |
+| ----------------------- | ----------- | ----------- | ------- | ------- | ------- |
+| **Detector/Descriptor** | BRIEF       | ORB         | FREAK   | AKAZE   | SIFT    |
+| SHI-TOMASI              | 15.9493     | 16.0352     | 46.2981 | X       | 24.3293 |
+| HARRIS                  | 16.1311     | 18.3939     | 49.146  | X       | 24.953  |
+| FAST                    | **2.76489** | **2.89337** | 37.4923 | X       | 15.4976 |
+| BRISK                   | **266.379** | 266.203     | 294.392 | X       | 287.341 |
+| ORB                     | 26.3151     | 23.6431     | 54.9679 | X       | 48.6818 |
+| AKAZE                   | 56.5897     | 58.8057     | 89.2376 | 102.072 | 75.7282 |
+| SIFT                    | 64.4931     | X           | 100.142 | X       | 114.903 |
+
+**First Impression according to the above table statistics:**
+
+| **Detector** | Descriptor | Pros                                                         | Cons                                      |
+| ------------ | ---------- | ------------------------------------------------------------ | ----------------------------------------- |
+| HARRIS       |            |                                                              | Less detected keypoints                   |
+| SHI-TOMASI   |            | Relatively many keypoints and  less detection time           |                                           |
+| FAST         |            | Very fast detection speed and large amount of detected keypoints |                                           |
+| BRISK        | BRISK      | Very good detection precious with larges amount of detected keypoints/ Less extraction time | Long detection time                       |
+| ORB          | ORB        | Relatively many keypoints and  less detection time/ Medium extraction time |                                           |
+| AKAZE        | AKAZE      |                                                              | Long detection time/ Long extraction time |
+|              | FREAK      |                                                              | Long extraction time                      |
+| SIFT         | SIFT       |                                                              | Long detection time/ Long extraction time |
+|              | BRIEF      | Less extraction time                                         |                                           |
+
+
+The initial impressions based on the table statistics are:
+
+**FAST + BRIEF**: Higher speed and relative good accuracy
+**BRISK + BRIEF**: Higher accuracy
+**FAST + ORB**: relatively good speed and accuracy
--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cmath>
 #include <limits>
+#include <numeric>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -41,9 +42,13 @@ int main(int argc, const char *argv[])
     bool bVis = false;            // visualize results
 
     /* MAIN LOOP OVER ALL IMAGES */
+    vector<double> det_record_time(10);
+    vector<int> det_record_num(10);
+    vector<double> des_record_time(10);
+    vector<double> mat_record_time(9);
+    vector<int> mat_record_num(9);
 
-    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex++)
-    {
+    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex++) {
         /* LOAD IMAGE INTO BUFFER */
 
         // assemble filenames for current index
@@ -62,6 +67,11 @@ int main(int argc, const char *argv[])
         // push image into data frame buffer
         DataFrame frame;
         frame.cameraImg = imgGray;
+
+        // remove old frame in left side of dataBuffer and add new frame in right side
+        if (dataBuffer.size() > dataBufferSize) {
+            dataBuffer.erase(dataBuffer.begin());
+        }
         dataBuffer.push_back(frame);
 
         //// EOF STUDENT ASSIGNMENT
@@ -71,20 +81,38 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "SHITOMASI";
 
-        //// STUDENT ASSIGNMENT
+//        string detectorType = "SHITOMASI";
+//         string detectorType = "HARRIS";
+         string detectorType = "FAST";
+//         string detectorType = "BRISK";
+//         string detectorType = "ORB";
+//         string detectorType = "AKAZE";
+//         string detectorType = "SIFT";
+
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
         //// -> HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
 
-        if (detectorType.compare("SHITOMASI") == 0)
+        // Time count for detector
+        double t_det = (double) cv::getTickCount();
+        if (detectorType.compare("SHITOMASI") == 0) 
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
+        } else if (detectorType.compare("HARRIS") == 0) {
+            detKeypointsHarris(keypoints, imgGray, false);
+
+        } else if (detectorType.compare("BRISK") == 0 || detectorType.compare("SIFT") == 0 ||
+                   detectorType.compare("AKAZE") == 0 || detectorType.compare("ORB") == 0 ||
+                   detectorType.compare("FAST") == 0) {
+            detKeypointsModern(keypoints, imgGray, detectorType, false);
+        } else {
+            throw invalid_argument(detectorType + " is not supported");
         }
-        else
-        {
-            //...
-        }
+
+        t_det = ((double) cv::getTickCount() - t_det) / cv::getTickFrequency();
+        t_det = 1000 * t_det / 1.0;
+        cout << detectorType << " detection in " << t_det << " ms" << endl;
+
         //// EOF STUDENT ASSIGNMENT
 
         //// STUDENT ASSIGNMENT
@@ -93,21 +121,36 @@ int main(int argc, const char *argv[])
         // only keep keypoints on the preceding vehicle
         bool bFocusOnVehicle = true;
         cv::Rect vehicleRect(535, 180, 180, 150);
+        vector<cv::KeyPoint> insidePoints;
         if (bFocusOnVehicle)
-        {
-            // ...
+         {
+            for (auto keypt:keypoints) {
+                bool isinside = vehicleRect.contains(keypt.pt);
+                if (isinside) {
+                    insidePoints.push_back(keypt);
+                }
+            }
+            keypoints = insidePoints;
         }
+        cout << "Number of keypoints on the preceding vehicle:" << keypoints.size() << endl;
+        det_record_num.push_back(keypoints.size());
+        det_record_time.push_back(t_det);
 
-        //// EOF STUDENT ASSIGNMENT
+        // draw figure of the keypoints on the preceding vehicle
+        if (false) {
+            cv::Mat visImage = img.clone();
+            cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            string windowName = "Keypoints on the preceding vehicle";
+            cv::namedWindow(windowName);
+            imshow(windowName, visImage);
+            cv::waitKey(0);
+        }
 
         // optional : limit number of keypoints (helpful for debugging and learning)
         bool bLimitKpts = false;
-        if (bLimitKpts)
-        {
+        if (bLimitKpts) {
             int maxKeypoints = 50;
-
-            if (detectorType.compare("SHITOMASI") == 0)
-            { // there is no response info, so keep the first 50 as they are sorted in descending quality order
+            if (detectorType.compare("SHITOMASI") ==0) { // there is no response info, so keep the first 50 as they are sorted in descending quality order
                 keypoints.erase(keypoints.begin() + maxKeypoints, keypoints.end());
             }
             cv::KeyPointsFilter::retainBest(keypoints, maxKeypoints);
@@ -116,7 +159,7 @@ int main(int argc, const char *argv[])
 
         // push keypoints and descriptor for current frame to end of data buffer
         (dataBuffer.end() - 1)->keypoints = keypoints;
-        cout << "#2 : DETECT KEYPOINTS done" << endl;
+       cout << "#2 : DETECT KEYPOINTS done" << endl;
 
         /* EXTRACT KEYPOINT DESCRIPTORS */
 
@@ -125,9 +168,22 @@ int main(int argc, const char *argv[])
         //// -> BRIEF, ORB, FREAK, AKAZE, SIFT
 
         cv::Mat descriptors;
-        string descriptorType = "BRISK"; // BRIEF, ORB, FREAK, AKAZE, SIFT
-        descKeypoints((dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->cameraImg, descriptors, descriptorType);
-        //// EOF STUDENT ASSIGNMENT
+
+        
+        string descriptorType = "BRIEF"; // BRIEF, ORB, FREAK, AKAZE, SIFT
+//         string descriptorType = "ORB";
+//         string descriptorType = "FREAK";
+//         string descriptorType = "AKAZE";
+//         string descriptorType = "SIFT";
+
+        double t_des = (double) cv::getTickCount();
+        descKeypoints((dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->cameraImg, descriptors,
+                      descriptorType);
+        t_des = ((double) cv::getTickCount() - t_des) / cv::getTickFrequency();
+        t_des = 1000 * t_des / 1.0;
+        cout << descriptorType << " descriptor extraction in " << t_des << " ms" << endl;
+
+        des_record_time.push_back(t_des);
 
         // push descriptors for current frame to end of data buffer
         (dataBuffer.end() - 1)->descriptors = descriptors;
@@ -140,29 +196,46 @@ int main(int argc, const char *argv[])
             /* MATCH KEYPOINT DESCRIPTORS */
 
             vector<cv::DMatch> matches;
-            string matcherType = "MAT_BF";        // MAT_BF, MAT_FLANN
-            string descriptorType = "DES_BINARY"; // DES_BINARY, DES_HOG
-            string selectorType = "SEL_NN";       // SEL_NN, SEL_KNN
 
-            //// STUDENT ASSIGNMENT
+            string matcherType = "MAT_BF";        // brute force (BF)
+//             string matcherType = "MAT_FLANN";     // Fast Library for Approximate Nearest Neighbors (FLANN)
+
+            // BINARY descriptors :BRISK, BRIEF, ORB, FREAK, and AKAZE
+            // HOG descriptors : SIFT (and SURF and GLOH, all patented).
+            string descriptorclass{};
+            if (descriptorType.compare("SIFT")==0) {
+                descriptorclass = "DES_HOG";
+
+            } else {
+                descriptorclass = "DES_BINARY";
+            }
+
+//             string selectorType = "SEL_NN"; // choose nearest neighbors (NN)
+            string selectorType = "SEL_KNN";   // k nearest neighbors (KNN)
+
+
             //// TASK MP.5 -> add FLANN matching in file matching2D.cpp
             //// TASK MP.6 -> add KNN match selection and perform descriptor distance ratio filtering with t=0.8 in file matching2D.cpp
-
+            double t_mat = (double) cv::getTickCount();
             matchDescriptors((dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints,
                              (dataBuffer.end() - 2)->descriptors, (dataBuffer.end() - 1)->descriptors,
-                             matches, descriptorType, matcherType, selectorType);
+                             matches, descriptorclass, matcherType, selectorType);
+            t_mat = ((double) cv::getTickCount() - t_mat) / cv::getTickFrequency();
+            t_mat = 1000 * t_mat / 1.0;
 
-            //// EOF STUDENT ASSIGNMENT
+            cout << matcherType << " descriptor matching in " << t_mat << " ms" << endl;
+            cout<<endl;
+            mat_record_num.push_back(matches.size());
+            mat_record_time.push_back(t_mat);
 
             // store matches in current data frame
             (dataBuffer.end() - 1)->kptMatches = matches;
 
-            cout << "#4 : MATCH KEYPOINT DESCRIPTORS done" << endl;
+//            cout << "#4 : MATCH KEYPOINT DESCRIPTORS done" << endl;
 
             // visualize matches between current and previous image
             bVis = true;
-            if (bVis)
-            {
+            if (bVis) {
                 cv::Mat matchImg = ((dataBuffer.end() - 1)->cameraImg).clone();
                 cv::drawMatches((dataBuffer.end() - 2)->cameraImg, (dataBuffer.end() - 2)->keypoints,
                                 (dataBuffer.end() - 1)->cameraImg, (dataBuffer.end() - 1)->keypoints,
@@ -171,15 +244,29 @@ int main(int argc, const char *argv[])
                                 vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
                 string windowName = "Matching keypoints between two camera images";
-                cv::namedWindow(windowName, 7);
+                cv::namedWindow(windowName);
                 cv::imshow(windowName, matchImg);
                 cout << "Press key to continue to next image" << endl;
+                cout << endl;
                 cv::waitKey(0); // wait for key to be pressed
             }
             bVis = false;
         }
 
     } // eof loop over all images
+    double ave_det_time=double(accumulate(det_record_time.begin(), det_record_time.end(), 0.) / 10.0);
+    double ave_det_num=double(accumulate(det_record_num.begin(), det_record_num.end(), 0.) / 10.0);
+    double ave_des_time=double(accumulate(des_record_time.begin(), des_record_time.end(), 0.) / 10.0);
+    double ave_mat_time=double(accumulate(mat_record_time.begin(), mat_record_time.end(), 0.) / 9.0);
+    double ave_mat_num=double(accumulate(mat_record_num.begin(), mat_record_num.end(), 0.) / 9.0);
+    double ave_total_time=ave_des_time+ave_det_time+ave_mat_time;
+
+    cout << "Average detection time: " <<ave_det_time<< endl;
+    cout << "Average detection num: " <<ave_det_num<< endl;
+    cout << "Average description time: "<< ave_des_time << endl;
+    cout << "Average matching time: " << ave_mat_time<< endl;
+    cout << "Average matching num: " << ave_mat_num<< endl;
+    cout << "Average total time: " << ave_total_time<< endl;
 
     return 0;
 }
